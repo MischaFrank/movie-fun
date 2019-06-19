db73708edf7bf3598dc6f3df64ddf393fc4506dc
Henry Scherlo <hscherlo@gmail.com>
2019-06-19 13:37:58 +0200

    background jobs using db

--- src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
+++ src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
@@ -2,10 +2,16 @@ package org.superbiz.moviefun.albums;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.sql.DataSource;
+import java.sql.Date;
+import java.sql.Timestamp;
 
 @Configuration
 @EnableAsync
@@ -18,21 +24,36 @@ public class AlbumsUpdateScheduler {
     private final AlbumsUpdater albumsUpdater;
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    public AlbumsUpdateScheduler(AlbumsUpdater albumsUpdater) {
+    private JdbcTemplate jdbcTemplate;
+
+    public AlbumsUpdateScheduler(AlbumsUpdater albumsUpdater, @Autowired DataSource dataSource) {
         this.albumsUpdater = albumsUpdater;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
 
-    @Scheduled(initialDelay = 15 * SECONDS, fixedRate = 2 * MINUTES)
+    @Scheduled(initialDelay = 2 * SECONDS, fixedRate = 5000) //fixedRate = 2 * MINUTES)
     public void run() {
         try {
-            logger.debug("Starting albums update");
-            albumsUpdater.update();
+            if (startAlbumSchedulerTask()) {
+                logger.debug("Starting albums update");
+                albumsUpdater.update();
+                logger.debug("Finished albums update");
 
-            logger.debug("Finished albums update");
+            } else {
+                logger.debug("Nothing to start");
+            }
 
         } catch (Throwable e) {
             logger.error("Error while updating albums", e);
         }
+    }
+
+    private boolean startAlbumSchedulerTask() {
+        int affectedRows = this.jdbcTemplate.update("UPDATE album_scheduler_task SET started_at = ? WHERE started_at < ? OR started_at is null;", new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis() - 12000));
+        if (affectedRows > 1) {
+            throw new IllegalStateException("Mehr als eine Zeile in der Tabelle album_scheduler vorhanden.");
+        }
+        return affectedRows > 0;
     }
 }
